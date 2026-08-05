e131e0e7215441af2d3e5d67d73732a5b93d63bb
BoBJazz <62244631+Mtjzw@users.noreply.github.com>
2026-08-05 13:49:47 +0200

    Add files via upload

---
+++ assets/js/uslugi.js
@@ -0,0 +1,28 @@
+const uslugi = {
+  oczyszczanieWodorowe: {
+    cena: 180,
+    czas: 60
+  },
+  kawitacja: {
+    cena: 100,
+    czas: 60
+  },
+  infuzjaTlenowa: {
+    cena: 160,
+    czas: 60
+  },
+  manicureHybrydowy: {
+    cena: 130,
+    czas: 80
+  },
+  pedicure: {
+    cena: 130,
+    czas: 90
+  },
+  makijaz: {
+    cena: 250,
+    czas: 0
+  }
+};
+
+
