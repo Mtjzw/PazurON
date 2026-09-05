d055c41aa4491f90376f591bc93013dcf2b8d210
BoBJazz <62244631+Mtjzw@users.noreply.github.com>
2026-09-05 20:26:38 +0200

    Update service duration times in uslugi.js

--- assets/js/uslugi.js
+++ assets/js/uslugi.js
@@ -1,19 +1,19 @@
 const uslugi = {
   oczyszczanieWodorowe: {
     cena: 180,
-    czas: 60
+    czas: 80
   },
   kawitacja: {
     cena: 100,
-    czas: 60
+    czas: 80
   },
   infuzjaTlenowa: {
     cena: 160,
-    czas: 60
+    czas: 80
   },
   manicureHybrydowy: {
     cena: 130,
-    czas: 80
+    czas: 90
   },
   pedicure: {
     cena: 130,
